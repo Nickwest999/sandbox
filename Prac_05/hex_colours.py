81e1f4aa68cd784a4114a80b202e5c296309b49e
Nicholas <nicholas.west1@my.jcu.edu.au>
2020-10-14 12:49:38 +1000

    Prac_05 Completed

---
+++ Prac_05/hex_colours.py
@@ -0,0 +1,18 @@
+"""
+10/9/2020
+Nicholas West
+Hex Colours
+"""
+
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+                "Brown2": "#ee3b3b", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+                "aquamarine4": "#458b74", "azure1": "#f0ffff",
+                "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
+                "beige": "#f5f5dc", "CadetBlue2": "#8ee5ee"}
+
+colour_name = input("Enter a colour name: ")
+while colour_name != "":
+    print("The code for \"{}\" is {}".format(colour_name, COLOUR_CODES.get(colour_name)))
+    colour_name = input("Enter a colour name: ")
